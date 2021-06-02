1a170dc2d6828cc59ab13c7f40dc5d1521b2a0df
alfredsmarteze <alfredsmarteze@outlook.com>
2021-06-02 16:25:05 +0100

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+
+    //Toggle About
+    $(".hideabout").hide()
+        $("#about").on("click", function () {
+        if ($(".hideabout").is(":hidden")) {
+            $(".hideabout").fadeToggle("fast")
+        }
+        else if ($(".hideabout").is(":visible"))
+        {
+            $(".hideabout").fadeToggle("fast")
+        }
+
+        })
+
+    $("#contact").on("click", function () {
+
+        window.location="Home/index/#contacthere"
+    })
+
+})
